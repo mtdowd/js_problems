a2ef6dd8bf12495eec11669327faf1fbe03f5e9a
MTD <mtdowd@gmail.com>
2014-10-28 14:05:57 -0400

    Built photo guessing game webpage
    * Created html and js file
    * No jQuery used

---
+++ guess_game.js
@@ -0,0 +1,45 @@
+
+var images = [
+  {kind: "monkey", src: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRlaFFuiq4RSfLOiqPCohSLIJh-M-wLCWWWotE_G2A2vGmHY8V4" },
+  {kind: "monkey", src: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRzA3NkPO5_QO57zA9Y9FliY06OUoR8bu5qrFb6FaBI4NtsgowyEeSmFfaB" },
+  {kind: "monkey", src: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQs-BD_zQ97Qnrfq7PrxRTt_wnbNQoxgCPNHT94Iz18TC09EKJO78zOOm3w" },
+  {kind: "penguin", src: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTS0nw-AnTdHYuu0DVwIrPOn7raC1BKrocaQ_YPYiPaLDkOU3Slsw" },
+  {kind: "penguin", src: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcS0qVKatgiAd_nK89K9OVl4nal4nYd6-Kj9VZvG2Zp-0rcxSlmbgQ" },
+  {kind: "penguin", src: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTknxHM9ZrxsOmOvobWAIdM1Eq6i7qD3-0RUG64V8TL-zzK5D_F" }
+]
+
+var correctGuesses = 0
+var currentImage = 0
+
+window.onload = loadImage;
+
+function loadImage() {
+  var imageTag = document.getElementById("guessing-image");
+    if (images[currentImage] !== undefined) {
+      imageTag.src = images[currentImage].src;
+    } else {
+      completeGame();
+    }
+}
+
+function guess(kind){
+  if (images[currentImage].kind === kind) {
+    correctGuesses += 1;
+  }
+  updateResults();
+  updateImage();
+}
+
+function updateImage(){
+  currentImage += 1;
+  loadImage();
+}
+
+function updateResults(){
+  var resultTag = document.getElementById("correct-guesses");
+  resultTag.innerHTML = correctGuesses;
+}
+
+function completeGame() {
+  window.alert("Congratulations. You won " + correctGuesses + " out of " + images.length);
+}
